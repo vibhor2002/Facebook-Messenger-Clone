693dc6dea023126bc9623a741d50f062e07ebaca
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-16 18:39:03 +0530

    creating input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React , { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
       <h1>Hello There!!!</h1>
+
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button>Send Message</button>
+      {/* Input Field */}
+      {/* Button */}
+
+      {/* Messages */}
     </div>
   );
 }
