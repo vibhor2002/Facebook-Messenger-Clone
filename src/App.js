9dee829829868e6afb5370712a1cc4bfbfae0d2c
Vibhor Grover <grovervibhor21@gmail.com>
2022-05-16 23:34:18 +0530

    Add ability to send messages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import './App.css';
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [input, setInput] = useState('');
-  
+  const [messages, setMessages] = useState([]);
+
   console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // all the logic to send the message goes here
+    setMessages([...messages, input]);
+    setInput('');
+  }
 
   return (
     <div className="App">
       <h1>Hello There!!!</h1>
 
-
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button>Send Message</button>
-      {/* Input Field */}
-      {/* Button */}
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
 
       {/* Messages */}
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
