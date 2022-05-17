8bf20f191618a9debc01f44daeb3aaabac979bcd
Vibhor Grover <grovervibhor21@gmail.com>
2022-05-18 01:25:33 +0530

    some updates to the component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import Message from './Message';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [username, setUsername] = useState('');
+  const [messages, setMessages] = useState([
+    {username: 'vibhu',text: 'hey guys'},
+    {username: 'lakshay',text: 'hey there!!'}]);
+  const [username, setUsername] = useState(' ');
+
+  // use state = variable in react
+  // use effect = run on condition
+
+  useEffect(() => {
+   setUsername(prompt('Please enter your name'));
+  },[])
+  
 
   const sendMessage = (event) => {
     event.preventDefault();
     // all the logic to send the message goes here
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput('');
   }
 
   return (
     <div className="App">
       <h1>Hello There!!!</h1>
+      <h2>Welcome {username}</h2>
 
       <form>
         <FormControl>
@@ -31,7 +42,7 @@ function App() {
 
       {
         messages.map(message => (
-            <Message text={message} />
+            <Message username={message.username} text={message.text} />
         ))
       }
     </div>
--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,21 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
 
 function Message(props) {
-  return (
-    <div>
-        <h2>{props.text}</h2>
-    </div>
-  )
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography
+                        color="white"
+                        variant="h5"
+                        component="h2">
+                            {props.username}: {props.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+         </div>
+    )
 }
 
 export default Message
